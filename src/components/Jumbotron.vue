ca9ac95158e29ec17728352dccfd88b48a14bba7
pietrolupi <pietro.lupi92@gmail.com>
2023-10-19 14:01:00 +0200

    [add]: jumbotron statico

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,6 +1,7 @@
 
 <script>
 
+import { store } from '../data/store';
 
 
 export default {
@@ -9,7 +10,7 @@ export default {
 
   data(){
     return{
-      
+      store,
 
     }
   }
@@ -20,10 +21,23 @@ export default {
 <template>
  
     <div class="jumbotron">
-      <nav>
+      <nav class="d-flex justify-content-between ">
+
         <img src="../assets/img/logo-light.png" alt="">
-        
+
+        <ul class="d-flex ">
+          <li v-for="(voice,index) in store.jumboMenu"
+          :key="index"
+          v-html="voice"
+          ></li>
+        </ul>
       </nav>
+
+      <div class="jumbo-main">
+        <h1>Contemporary Ideas</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi voluptas culpa animi doloribus consequatur possimus autem eum, voluptatum iure est similique facilis, voluptates </p>
+        <button>REGISTER NOW</button>
+      </div>
     </div>
  
  
@@ -31,9 +45,57 @@ export default {
 
 
 <style lang="scss" scoped>
+
+@use '../scss/partials/vars' as *;
   .jumbotron{
     background-image: url('../assets/img/h5-slide-3-background.jpg');
     height: 820px;
     width: 100vw;
+    color: $text-secondary-color;
+    position: relative;
+    nav{
+      align-items: center;
+      padding: 45px;
+
+      img{
+        width: 200px;
+      }
+      ul{
+        margin: 0;
+        padding: 0;
+        li{
+          
+          
+          display: inline-block;
+          margin: 0px 15px;
+          margin-bottom: -7px;
+          cursor: pointer;
+        }
+      }
+    }
+
+    .jumbo-main{
+      text-align: center;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 70%;
+      h1{
+        font-size: 4.5rem;
+        font-weight: 600;
+        margin-bottom: 30px;
+      }
+      p{
+        line-height: 30px;
+      }
+      button{
+        background-color: $color-tertiary;
+        color: $text-secondary-color;
+        border: none;
+        padding: 19px 41px ;
+      }
+    }
+
   }
 </style>
--- src/data/store.js
+++ src/data/store.js
@@ -3,5 +3,16 @@ import {reactive} from 'vue';
 
 export const store = reactive({
   
-  
+  jumboMenu: [
+    'HOME',
+    'COURSES',
+    'INSTRUCTORS',
+    'EVENTS',
+    'PAGES',
+    'ELEMENTS',
+    '<i class="fa-solid fa-magnifying-glass"></i>',
+    '<i class="fa-solid fa-file"></i>',
+    '<i class="fa-solid fa-bars"></i>'
+
+  ]
 })
